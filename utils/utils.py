4c90ba4d70094e00425ba434c11683499c104916
younesZdDz <fy_zadi@esi.dz>
2019-05-15 14:23:40 +0100

    [ADD] averager helper function

--- utils/utils.py
+++ utils/utils.py
@@ -38,3 +38,21 @@ class TextProcessor:
                 ret.append(self.stemmed[word])
         return ' '.join(ret)
 
+
+class Averager:
+    """
+    Small class useful for computing averaged features values
+    """
+
+    def __init__(self):
+        self.sum = 0
+        self.cnt = 0
+
+    def upd(self, val):
+        self.sum += val
+        self.cnt += 1
+
+    def get(self):
+        if self.cnt == 0:
+            return None
+        return self.sum / self.cnt
